06d835814e64f19a23412e169206903efe2f36c7
lloydkirk <kirklloyd@gmail.com>
2017-12-14 09:56:06 +0000

    generate jobs, and add a jitter

--- main.go
+++ main.go
@@ -3,8 +3,17 @@ package main
 import (
 	"flag"
 	"fmt"
+	"math/rand"
 	"os"
+	"path"
+	"path/filepath"
+	"time"
 )
+
+func randomMilliseconds(min, max int) float64 {
+	randInt := rand.Intn(max-min) + min
+	return (time.Millisecond * time.Duration(randInt)).Seconds()
+}
 
 func main() {
 	var (
@@ -12,7 +21,6 @@ func main() {
 	)
 
 	flag.StringVar(&workflowDir, "wfdir", "", "directory for job to exist")
-
 	flag.Parse()
 
 	if workflowDir == "" {
@@ -23,15 +31,60 @@ func main() {
 
 	wf := newWorkflow(workflowDir)
 
-	AddJob(wf, `sleep 3`)
-	AddJob(wf, `sleep 6`)
-	AddJob(wf, `sleep 8`)
-	AddJob(wf, `echo wef | sed -re's/(w)(e)f/\2\1/'`)
-	AddJob(wf, `echo bef`)
-	AddJob(wf, `echo lef`)
-	AddJob(wf, `ls -la`)
-	AddJob(wf, `pwd`)
-	AddJob(wf, `exit 1`)
+	// cmds := []string{
+	// 	`sleep 3`,
+	// 	`sleep 6`,
+	// 	`sleep 8`,
+	// 	`echo wef | sed -re's/(w)(e)f/\2\1/'`,
+	// 	`echo bef`,
+	// 	`echo lef`,
+	// 	`ls -la`,
+	// 	`pwd`,
+	// 	`exit 1`,
+	// }
+
+	// for _, c := range cmds {
+	// 	wf.AddJob(newJob(wf, []string{}, []Dependency{}, c))
+	// }
+
+	samples := []string{"a", "b", "c", "d"}
+	deps := []string{"e", "f", "g", "h"}
+	replace := []string{"hello", "this", "is", "the"}
+
+	tmpDir := "test/job/tmp"
+	for i, s := range samples {
+
+		sampleTmp := path.Join(tmpDir, s)
+
+		tmpFile, _ := filepath.Abs(path.Join(sampleTmp, s))
+		depTmp, _ := filepath.Abs(path.Join(sampleTmp, deps[i]))
+
+		sampleCmd := fmt.Sprintf(
+			`echo "hello this is the world speaking" > %s && sleep %g`,
+			tmpFile, randomMilliseconds(1, 5000))
+
+		depCmd := fmt.Sprintf(`sed -e's|%s||g' %s > %s`,
+			replace[i], tmpFile, depTmp)
+
+		depJob := newJob(
+			wf,
+			[]string{sampleTmp},
+			[]*Job{},
+			[]string{depTmp},
+			false,
+			depCmd,
+		)
+
+		j := newJob(
+			wf,
+			[]string{sampleTmp},
+			[]*Job{depJob},
+			[]string{tmpFile},
+			false,
+			sampleCmd,
+		)
+		wf.AddJob(j)
+	}
 
 	os.Exit(wf.Run())
 }
