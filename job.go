227fb28d3ab261e975972dc287e16dca09eeb948
lloydkirk <kirklloyd@gmail.com>
2017-12-14 09:57:00 +0000

    adds dependencies and file tracking

--- job.go
+++ job.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	// "bytes"
-	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
@@ -31,22 +29,30 @@ func (j *JobState) increment() int {
 }
 
 type Job struct {
-	ID          int      `json:"id"`
-	Directories []string `json:"directories"`
-	Cmd         string   `json:"cmd"`
-	workflow    *Workflow
+	workflow *Workflow
+
+	ID           int      `json:"id"`
+	Directories  []string `json:"directories"`
+	Dependencies []*Job   `json:"dependencies"`
+	Produces     []string `json:"produces"`
+	CleanTmp     bool     `json:"clean_tmp"`
+	Cmd          string   `json:"cmd"`
 }
 
-func AddJob(wf *Workflow, cmd string) {
+func newJob(wf *Workflow, dirs []string, deps []*Job, produces []string, clean bool, cmd string) *Job {
 	jobId := jState.increment()
-
-	wf.Jobs = append(wf.Jobs, &Job{jobId, []string{}, templateExecutable(cmd), wf})
+	return &Job{wf, jobId, dirs, deps, produces, clean, templateExecutable(cmd)}
 }
 
-func (j *Job) initJob() {
+func (j *Job) initJob() error {
 	j.createJobDirs()
 	j.writeCommandScript()
-	j.createDirectories()
+	err := j.createDirectories()
+	return err
+}
+
+func (j *Job) AddDependency(deps ...*Job) {
+	j.Dependencies = append(j.Dependencies, deps...)
 }
 
 func (j *Job) pathToExec(s ...string) string {
@@ -76,14 +82,25 @@ func (j *Job) createJobDirs() {
 	}
 }
 
-func (j *Job) createDirectories() {
+func (j *Job) createDirectories() (err error) {
 	for _, d := range j.Directories {
-		fmt.Println("creating: ", d)
-		err := os.MkdirAll(d, 0755)
-		if err != nil {
-			log.Fatal(err)
+		exists, err := Exists(d)
+		switch {
+		case err != nil:
+			log.Printf("Failed to stat dir '%s' job_id:%d error:'%s'", d, j.ID, err.Error())
+			return err
+		case exists:
+			return nil
+		default:
+			log.Println("creating: ", d)
+			err = os.MkdirAll(d, 0755)
+			if err != nil {
+				return err
+			}
 		}
+
 	}
+	return
 }
 
 func (j *Job) writeCommandScript() {
@@ -105,15 +122,39 @@ func (j *Job) openLogs() (outLog, errLog *os.File, err error) {
 	return outLog, errLog, nil
 }
 
+func (j *Job) checkProduces() bool {
+	if len(j.Produces) == 0 {
+		return false
+	}
+	for _, f := range j.Produces {
+		exists, err := Exists(f)
+		if err != nil {
+			log.Printf("Failed to stat file '%s' job_id:%d error:'%s'", f, j.ID, err.Error())
+			j.workflow.JobsFailed += 1 // TODO: thread safe?
+			return exists
+		}
+		if !exists {
+			return exists
+		}
+	}
+	return true
+}
+
 func (j *Job) runJob(wg *sync.WaitGroup) {
 	defer wg.Done()
+	if j.checkProduces() {
+		return
+	}
+
 	outLog, errLog, err := j.openLogs()
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	defer outLog.Close()
 	defer errLog.Close()
 
+	depWg := &sync.WaitGroup{}
 	cmd := exec.Command(j.pathToExec("exe"))
 
 	cmd.Stdout = outLog
@@ -127,4 +168,11 @@ func (j *Job) runJob(wg *sync.WaitGroup) {
 	}
 
 	log.Println("Job Succeeded: job_id:", j.ID)
+
+	for _, d := range j.Dependencies {
+		d.initJob()
+		depWg.Add(1)
+		go d.runJob(wg)
+	}
+	depWg.Wait()
 }
