9c8d91de42c90dcf7653fb4cdd29af225d84f37c
lloydkirk <kirklloyd@gmail.com>
2018-06-09 23:39:07 +0100

    #1: replace tsv implementation with boltdb

--- event_file.go
+++ event_file.go
@@ -1,84 +1,36 @@
 package main
 
 import (
-	"crypto/sha256"
-	"encoding/csv"
 	"fmt"
-	"io"
-	"os"
 	"strconv"
-	"time"
+
+	"github.com/boltdb/bolt"
 )
 
-// BackGroundPollMs is the rate (in ms) by which the backgroundWriteProcess
-// will poll the channel to write to the event file log
-const BackGroundPollMs = 100
-
-// EventFileHeader specifies the fields that a record in the event file contains
-// var EventFileHeader = []string{"job_id", "start_time_ns", "end_time_ns", "arg_hash"}
-
-// EventFileEntry describes a record to be written to the event file
-type EventFileEntry struct {
-	JobID     int
-	StartTime int64
-	EndTime   int64
-	ArgHash   [sha256.Size]byte
-}
-
-func newEventFileWriter(iw io.Writer) *csv.Writer {
-	w := csv.NewWriter(iw)
-	w.Comma = '\t'
-	return w
-}
-
-func newEventFile(p string) (f *os.File, err error) {
-	return os.OpenFile(p, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-}
-
-func backgroundWriteProcess(iw io.Writer) *chanWriter {
-	writer := newChanWriter(iw)
-	go func() {
-		for {
-			for efl := range writer.Chan() {
-				writer.Write(efl)
-			}
-			time.Sleep(time.Millisecond * BackGroundPollMs)
+func setupEventDB(f string) (db *bolt.DB, err error) {
+	db, err = bolt.Open(f, 0600, nil)
+	if err != nil {
+		err = fmt.Errorf("could not open event file: %s", err)
+		return
+	}
+	err = db.Update(func(tx *bolt.Tx) (err error) {
+		_, err = tx.CreateBucketIfNotExists([]byte("Job"))
+		if err != nil {
+			err = fmt.Errorf("could not create Job bucket: %s", err)
+			return
 		}
-	}()
-	return writer
-}
-
-type chanWriter struct {
-	ch     chan EventFileEntry
-	writer *csv.Writer
-}
-
-func newChanWriter(iw io.Writer) *chanWriter {
-	return &chanWriter{
-		ch:     make(chan EventFileEntry),
-		writer: newEventFileWriter(iw),
-	}
-}
-
-func (w *chanWriter) Chan() <-chan EventFileEntry {
-	return w.ch
-}
-
-func (w *chanWriter) Close() error {
-	close(w.ch)
-	return nil
-}
-
-func (w *chanWriter) Write(efe EventFileEntry) error {
-	w.writer.Write([]string{
-		strconv.Itoa(efe.JobID),
-		strconv.FormatInt(efe.StartTime, 10),
-		strconv.FormatInt(efe.EndTime, 10),
-		fmt.Sprintf("%x", efe.ArgHash),
+		return
 	})
-	w.writer.Flush()
-	if err := w.writer.Error(); err != nil {
-		return err
+	if err != nil {
+		err = fmt.Errorf("could not set up buckets: %s", err)
+		return
 	}
-	return nil
+	return
+}
+
+func addJob(db *bolt.DB, arghash []byte, returnValue int) error {
+	return db.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte("Job"))
+		return b.Put(arghash, []byte(strconv.Itoa(returnValue)))
+	})
 }
--- event_file_test.go
+++ event_file_test.go
@@ -1,80 +1,14 @@
 package main
 
-import (
-	"bytes"
-	"crypto/sha256"
-	"fmt"
-	"strconv"
-	"strings"
-	"testing"
-	"time"
+// import (
+// 	"testing"
+// )
 
-	"github.com/boltdb/bolt"
-)
-
-func TestEventFile(t *testing.T) {
-	b := bytes.Buffer{}
-	bwp := backgroundWriteProcess(&b)
-	defer bwp.Close()
-
-	command := "ls -la"
-	sum := sha256.Sum256([]byte(command))
-	startTime := time.Now().UnixNano()
-	endtime := time.Now().UnixNano()
-	jobEntry := strings.Join([]string{
-		"1", strconv.FormatInt(startTime, 10), strconv.FormatInt(endtime, 10),
-		"1de700c29687cae34561545f50d3c8b3d9afe88e04cc11069f8a6dc6e4ce9464"}, "\t")
-
-	expectedFileContents := bytes.Buffer{}
-	expectedFileContents.WriteString(jobEntry)
-	expectedFileContents.WriteRune('\n')
-
-	efe := EventFileEntry{JobID: 1, StartTime: startTime, EndTime: endtime, ArgHash: sum}
-	bwp.Write(efe)
-	if b.String() != expectedFileContents.String() {
-		t.Errorf("Unexpected test file contents:\nExpected:\n>>>%s<<<\nGot:\n>>>%s<<<\n",
-			expectedFileContents.String(), b.String())
-	}
-}
-
-func setupEventFile() (db *bolt.DB, err error) {
-	db, err = bolt.Open("test.db", 0600, nil)
-	if err != nil {
-		err = fmt.Errorf("could not open event file: %s", err)
-		return
-	}
-	err = db.Update(func(tx *bolt.Tx) (err error) {
-		root, err := tx.CreateBucketIfNotExists([]byte("DB"))
-		if err != nil {
-			err = fmt.Errorf("could not create root bucket: %s", err)
-			return
-		}
-		_, err = root.CreateBucketIfNotExists([]byte("Job"))
-		if err != nil {
-			err = fmt.Errorf("could not create Job bucket: %s", err)
-			return
-		}
-		return
-	})
-	if err != nil {
-		err = fmt.Errorf("could not set up buckets: %s", err)
-		return
-	}
-	return
-}
-
-func addJob(db *bolt.DB, id int, arghash []byte) error {
-	return db.Update(func(tx *bolt.Tx) error {
-		b := tx.Bucket([]byte("DB")).Bucket([]byte("Job"))
-		return b.Put([]byte(id), arghash)
-	})
-}
-
-func TestBoltIntegration(t *testing.T) {
-	db, err := setupEventFile()
-	if err != nil {
-		t.Error(err)
-	}
-	defer db.Close()
-	addJob(db, 1, []byte("ohai"))
-}
+// func TestBoltIntegration(t *testing.T) {
+// 	db, err := setupEventFile()
+// 	if err != nil {
+// 		t.Error(err)
+// 	}
+// 	defer db.Close()
+// 	addJob(db, 1, []byte("ohai"))
+// }
